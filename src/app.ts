5e49a859d85bccde686757cfba365420c908ba88
Minyongggg <54466093+Minyongggg@users.noreply.github.com>
2022-06-21 19:22:05 +0900

    jenkins

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(helmet());
 
 // API Routing
 app.get("/", (req: Request, res: Response) =>
-  res.status(200).send(`HELLO Jenkins!!!!!`)
+  res.status(200).send(`HELLO Jenkins!!!!!!!`)
 );
 
 app.listen(8008, () => console.log(`listening on localhost: ${8008}`));
