ce96c197ecdbc9be0aded84357b428b8b1799174
Minyongggg <54466093+Minyongggg@users.noreply.github.com>
2022-06-21 18:33:52 +0900

    asdf

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import helmet from "helmet";
+import cors from "cors";
+
+const app: Application = express();
+
+// MiddleWares
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+// API Routing
+app.get("/", (req: Request, res: Response) =>
+  res.status(200).send(`HELLO Jenkins!`)
+);
+
+app.listen(8000, () => console.log(`listening on localhost: ${8000}`));
