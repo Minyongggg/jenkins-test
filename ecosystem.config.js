2c13a74cfda5e044c43353671de89412fba19543
Minyongggg <54466093+Minyongggg@users.noreply.github.com>
2022-06-21 18:36:58 +0900

    asdf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'hau-server',
-      script: 'build/app.js',
+      name: "jenkins-test",
+      script: "build/app.js",
       instances: 0,
-      exec_mode: 'cluster',
+      exec_mode: "cluster",
     },
   ],
 };
